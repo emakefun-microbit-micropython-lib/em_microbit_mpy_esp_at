f4ee52db74c1e3fff61b7ebb80b3e922615c4622
xianlonluohan <2047669296@qq.com>
2025-10-15 21:13:57 +0800

    优化程序，去掉不必要的代码

--- lib/esp_at_mqtt.py
+++ lib/esp_at_mqtt.py
@@ -1,9 +1,6 @@
 from esp_at_stream_util import *
 from micropython import const
 
-
-__version__ = "1.0.1"
-
 # <scheme>：
 OVER_TCP: int = const(1)
 OVER_TLS_NO_VERIFY: int = const(2)
@@ -26,15 +23,6 @@ class EspAtMqtt:
         return self._stream
 
     def _send_command(self, command: str, success_target: str, timeout_ms: int):
-        if not (
-            isinstance(command, str)
-            and command
-            and isinstance(success_target, str)
-            and success_target
-            and isinstance(timeout_ms, int)
-            and timeout_ms >= 0
-        ):
-            raise ValueError("Error: '_send_command' function, invalid parameters.")
         self._stream.write(command + "\r\n")
         targets = (
             success_target,
@@ -51,44 +39,16 @@ class EspAtMqtt:
         password: str,
         path: str,
     ):
-        if not (
-            isinstance(scheme, int)
-            and 1 <= scheme <= 10
-            and all(
-                isinstance(item, str) for item in (client_id, username, password, path)
-            )
-        ):
-            raise ValueError("Error: 'user_config' function, invalid parameters.")
         command = 'AT+MQTTUSERCFG=0,{},"{}","{}","{}",0,0,"{}"'.format(
             scheme, client_id, username, password, path
         )
         return self._send_command(command, "\r\nOK\r\n", 500)
 
     def connect_mqtt(self, host: str, port: int, reconnect: bool):
-        if not (
-            isinstance(host, str)
-            and host
-            and isinstance(port, int)
-            and 1 <= port <= 65535
-            and isinstance(reconnect, bool)
-        ):
-            raise ValueError("Error: 'user_config' function, invalid parameters.")
         command = 'AT+MQTTCONN=0,"{}",{},{}'.format(host, port, 1 if reconnect else 0)
         return self._send_command(command, "\r\nOK\r\n", 10000)
 
     def publish(self, topic: str, data: str, qos: int, retain: bool, timeout_ms: int):
-        if not (
-            isinstance(topic, str)
-            and topic
-            and isinstance(data, str)
-            and isinstance(qos, int)
-            and 0 <= qos <= 2
-            and isinstance(retain, bool)
-            and isinstance(timeout_ms, int)
-            and timeout_ms >= 0
-        ):
-            raise ValueError("Error: 'publish' function, invalid parameters.")
-
         data_bytes = data.encode("utf-8")
         command = 'AT+MQTTPUBRAW=0,"{}",{},{},{}'.format(
             topic, len(data_bytes), qos, 1 if retain else 0
@@ -103,28 +63,11 @@ class EspAtMqtt:
         return multi_find_util(self._stream, targets, timeout_ms) == 0
 
     def subscribe(self, topic: str, qos: int):
-        if not (
-            isinstance(topic, str) and topic and isinstance(qos, int) and 0 <= qos <= 2
-        ):
-            raise ValueError("Error: 'subscribe' function, invalid parameters.")
         command = 'AT+MQTTSUB=0,"{}",{}'.format(topic, qos)
         return self._send_command(command, "\r\nOK\r\n", 500)
 
-    def unsubscribe(self, topic: str):
-        if not (isinstance(topic, str) and topic):
-            raise ValueError("Error: 'unsubscribe' function, invalid parameter.")
-        return self._send_command(
-            'AT+MQTTUNSUB=0,"{}"'.format(topic), "\r\nOK\r\n", 500
-        )
-
-    def clean(self):
-        return self._send_command("AT+MQTTCLEAN=0", "\r\nOK\r\n", 500)
-
     def receive(self, timeout_ms: int):
-        if not (isinstance(timeout_ms, int) and timeout_ms >= 0):
-            raise ValueError("Error: 'receive' function, invalid parameter.")
-        header = '+MQTTSUBRECV:0,"'
-        if not single_find_util(self._stream, header, timeout_ms):
+        if multi_find_util(self._stream, ('+MQTTSUBRECV:0,"',), timeout_ms) != 0:
             return ("", 0)
         topic = read_until(self._stream, '"', 500)
         if topic is None or not skip_next(self._stream, ",", 500):
